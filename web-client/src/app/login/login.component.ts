0ea6abd6903c10ea65d84f9b1174a148b2f9f275
Jan Rubach <jan.rubach@ttsw.com.pl>
2023-05-24 21:26:10 +0200

    FM-03 - Przygotowanie formularza do logowania

--- web-client/src/app/login/login.component.ts
+++ web-client/src/app/login/login.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
   public emailPlaceholder: String = 'e-mail...';
   public passwordPlaceholder: String = 'hasło...';
   public loginValueButton: String = 'Zaloguj się';
+
+  public loginForm: FormGroup;
+
+  ngOnInit(): void {
+    this.loginForm = new FormGroup({
+      email: new FormControl(null),
+      password: new FormControl(null),
+    });
+  }
+
+  public onSubmit(): void {
+    console.log(this.loginForm);
+  }
 }
